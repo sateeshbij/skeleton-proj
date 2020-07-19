188a82822c4b807cd70b2617ffeb58423647daf3
sateeshb <sateesh.bijapuri@gmail.com>
2020-07-19 17:54:27 +0530

    Intitial math and its test scripts

---
+++ mycom/my_math.py
@@ -0,0 +1,8 @@
+def my_sum(a,b):
+    return a+b
+
+
+def my_multiply(a,b):
+    return a*b
+
+
---
+++ mytest/my_math_test.py
@@ -0,0 +1,4 @@
+from mycom.my_math import my_sum,my_multiply
+
+print('Sum results {}'.format(my_sum(2,3)))
+print("Multiply results {}".format(my_multiply(3,8)))
